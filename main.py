e18e6d51aabc0609c624a347775c3142305aa3a0
蔡福良 <946930866@qq.com>
2021-11-03 11:57:33 +0800

    init

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 # -*- coding: utf-8 -*-
+# Author: 蔡福良
 import argparse
 
 from proxy_server import ProxyServer
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='隧道代理服务器')
-    parser.add_argument('--listen_port', metavar='', help='设置服务器监听端口，默认使用18001', type=int, default=18001)
-    parser.add_argument('--bind', metavar='', help='设置绑定IP使用，默认使用0.0.0.0，任意IP可以连接使用', type=str, default='0.0.0.0')
+
+    epilog = '''当上游代理有Proxy-Authorization,请在请求时设置'''
+    parser = argparse.ArgumentParser(description='隧道代理服务器', epilog=epilog,prog='python main.py')
+    parser.add_argument('--listen_port', metavar='port', help='设置服务器监听端口，默认使用18001', type=int, default=18001)
+    parser.add_argument('--bind', metavar='ip', help='设置绑定IP使用，默认使用0.0.0.0，任意IP可以连接使用', type=str, default='0.0.0.0')
     args = parser.parse_args()
 
     ProxyServer.start(**{
